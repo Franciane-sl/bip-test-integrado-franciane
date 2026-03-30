bbf077d5f364b20d96e8f8b79e5f5ca8b9a94a3e
Franciane Sl <franciane.leitee@gmail.com>
2026-03-29 23:31:36 -0300

    feat: Define application routes for BeneficioForm and Transferencia components

--- frontend/bip-frontend/src/app/app.routes.ts
+++ frontend/bip-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BeneficioFormComponent } from './components/beneficio-form/beneficio-form';
+import { TransferenciaComponent } from './components/transferencia.component/transferencia.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BeneficioFormComponent }, 
+  { path: 'transferencia', component: TransferenciaComponent }, 
+];
